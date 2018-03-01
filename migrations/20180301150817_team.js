491e4d99042570b9c85c967aaa95cac18ce5b47b
Julia Niall <jnmareike@gmail.com>
2018-03-01 18:40:11 +1300

    add columns to table migrations, create database functions

--- migrations/20180301150817_team.js
+++ migrations/20180301150817_team.js
@@ -2,6 +2,7 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTableIfNotExists('team', table => {
     table.increments('id').primary()
     table.string('location')
+    table.integer('leader_id')
   })
 }
 exports.down = knex => knex.schema.dropTable('team')
