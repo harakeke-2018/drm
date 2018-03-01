84658721491027ae0dec5abc86851e84379f18e3
Julia Niall <jnmareike@gmail.com>
2018-03-02 10:55:47 +1300

    set-up db tests

---
+++ tests/stock.test.js
@@ -0,0 +1,30 @@
+/* global test expect beforeEach afterEach */
+
+const testEnv = require('./test-environment')
+
+const stock = require('../server/lib/stock')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = testEnv.getTestDb()
+  return testEnv.initialise(testDb)
+})
+
+afterEach(() => testEnv.cleanup(testDb))
+
+test('test harness is working', () => {
+  expect(true).toBeTruthy()
+})
+
+test('getStock returns all potential stock items', () => {
+  return stock.getStock(testDb)
+    .then(stock => {
+      expect(stock.length).toBe(3)
+    })
+})
+
+// test('receiveItems', () => {
+//   return stock.receiveItems(1, 1, testDb)
+//   .then(stock => {})
+// })
