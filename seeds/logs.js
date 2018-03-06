f9dcd486d9f9969f5870d3233c0b6572b8e535fd
Belma Gaukrodger <agilebelma@gmail.com>
2018-03-06 15:15:50 +1300

    added migrations and seeds

--- seeds/logs.js
+++ seeds/logs.js
@@ -1,10 +1,10 @@
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('log').del()
+  return knex('logs').del()
     .then(function () {
       // Inserts seed entries
-      return knex('log').insert([
+      return knex('logs').insert([
         {id: 1, location_stock_id: 1, past_location_id: 1, quantity_changed: 50, date: 'Wednesday 28 February 2018', activity: ''},
         {id: 2, location_stock_id: 2, past_location_id: 2, quantity_changed: 100, date: 'Thursday 1 March 2018', activity: ''},
         {id: 3, location_stock_id: 3, past_location_id: 3, quantity_changed: 80, date: 'Thursday 1 March 2018', activity: ''},
--- seeds/stock.js
+++ seeds/stock.js
@@ -5,9 +5,15 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('stock').insert([
-        {id: 1, item: 'water'},
-        {id: 2, item: 'blankets'},
-        {id: 3, item: 'tents'}
+        {id: 1, item: 'generator'},
+        {id: 2, item: 'radio'},
+        {id: 3, item: 'satellite'},
+        {id: 4, item: 'computer'},
+        {id: 5, item: 'printer'},
+        {id: 6, item: 'power'},
+        {id: 7, item: 'tools'},
+        {id: 8, item: 'butane'},
+        {id: 9, item: 'torch'}
       ])
     })
 }
