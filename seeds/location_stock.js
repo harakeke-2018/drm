90ff1681359e416a1b2a2d7ba7ca5f4daa274a55
Julia Niall <jnmareike@gmail.com>
2018-03-07 07:57:05 +1300

    add more seeds to display all items

--- seeds/location_stock.js
+++ seeds/location_stock.js
@@ -15,7 +15,15 @@ exports.seed = function (knex, Promise) {
 
         {id: 7, item_id: 1, location_id: 3, quantity: 20},
         {id: 8, item_id: 2, location_id: 3, quantity: 88},
-        {id: 9, item_id: 3, location_id: 3, quantity: 33}
+        {id: 9, item_id: 3, location_id: 3, quantity: 33},
+
+        {id: 10, item_id: 4, location_id: 1, quantity: 1},
+        {id: 11, item_id: 5, location_id: 1, quantity: 3},
+        {id: 12, item_id: 6, location_id: 2, quantity: 8},
+        {id: 13, item_id: 7, location_id: 3, quantity: 10},
+        {id: 14, item_id: 4, location_id: 2, quantity: 20},
+        {id: 15, item_id: 8, location_id: 3, quantity: 50},
+        {id: 16, item_id: 9, location_id: 1, quantity: 20}
       ])
     })
 }
