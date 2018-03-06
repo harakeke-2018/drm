cd9da17e706c114d08674211886bd5b1452e7667
Judy Ting-Edwards <judy.yun.ting@gmail.com>
2018-03-06 15:47:56 +1300

    added header

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 
+import Header from './Header'
 import Home from './Home'
 import LoginForm from './LoginForm'
 import Register from './RegisterForm'
@@ -8,6 +9,7 @@ import Register from './RegisterForm'
 const App = () => {
   return (
     <div>
+      <div><Header/></div>
       <Route exact path='/' component={LoginForm} />
       <Route path='/home' component={Home} />
       <Route exact path='/register' component={Register} />
